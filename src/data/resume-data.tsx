740c12cb7cd8eff55b571ed4716c68d8f733cbfe
Alec White <alexander.k.white@gmail.com>
2024-01-18 21:50:23 -0800

    make personal

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -1,278 +1,155 @@
 import {
-  AmbitLogo,
-  BarepapersLogo,
-  BimLogo,
-  CDGOLogo,
-  ClevertechLogo,
-  ConsultlyLogo,
-  EvercastLogo,
-  Howdy,
-  JarockiMeLogo,
-  JojoMobileLogo,
-  Minimal,
-  MobileVikingsLogo,
-  MonitoLogo,
-  NSNLogo,
-  ParabolLogo,
-  TastyCloudLogo,
-  YearProgressLogo,
+  WHOOPLogo,
+  MagsLogo,
+  CipherTechLogo,
+  WHOOPCoachLogo
 } from "@/images/logos";
 import { GitHubIcon, LinkedInIcon, XIcon } from "@/components/icons";
 
 export const RESUME_DATA = {
-  name: "Bartosz Jarocki",
-  initials: "BJ",
-  location: "Wrocław, Poland, CET",
-  locationLink: "https://www.google.com/maps/place/Wrocław",
+  name: "Alec White",
+  initials: "AW",
+  location: "San Francisco, PST",
+  locationLink: "https://www.google.com/maps/place/SanFrancisco",
   about:
-    "Full Stack Engineer focused on building products with extra attention to detail",
+    "Software Engineer, Full Stack & GenAI Application Layer",
   summary:
     "As a Full Stack Engineer, I have successfully taken multiple products from 0 to 1. I lead teams effectively, ensuring an environment where people can do their best work. Currently, I work mostly with TypeScript, React, Node.js, and GraphQL. I have over 8 years of experience in working remotely with companies all around the world.",
-  avatarUrl: "https://avatars.githubusercontent.com/u/1017620?v=4",
-  personalWebsiteUrl: "https://jarocki.me",
+  avatarUrl: "https://avatars.githubusercontent.com/u/5921175?v=4",
+  personalWebsiteUrl: "https://0xffffff.codes",
   contact: {
-    email: "bartosz.jarocki@hey.com",
-    tel: "+48530213401",
+    email: "alec@0xffffff.codes",
     social: [
       {
         name: "GitHub",
-        url: "https://github.com/BartoszJarocki",
+        url: "https://github.com/alec0xffffff",
         icon: GitHubIcon,
       },
       {
         name: "LinkedIn",
-        url: "https://www.linkedin.com/in/bjarocki/",
+        url: "https://www.linkedin.com/in/alec0xffffff/",
         icon: LinkedInIcon,
       },
       {
         name: "X",
-        url: "https://x.com/BartoszJarocki",
+        url: "https://x.com/alec0xffffff",
         icon: XIcon,
       },
     ],
   },
   education: [
     {
-      school: "Wrocław University of Technology",
-      degree: "Bachelor's Degree in Control systems engineering and Robotics",
-      start: "2007",
-      end: "2010",
+      school: "Wake Forest University",
+      degree: "Bachelor's Degree in Computer Science with a minor in Entrepreneurship & Social Enterprise",
+      start: "2013",
+      end: "2017",
     },
   ],
   work: [
     {
-      company: "Parabol",
-      link: "https://parabol.co",
-      badges: ["Remote"],
-      title: "Senior Full Stack Developer",
-      logo: ParabolLogo,
+      company: "WHOOP",
+      link: "https://whoop.com",
+      badges: ["Boston, MA"],
+      title: "Software Engineer II",
+      logo: WHOOPLogo,
       start: "2021",
-      end: "2024",
+      end: "current",
       description:
         "Implemented new features, led squad, worked on improving the way developers ship the code, started migration from Emotion to Tailwind CSS and more. Technologies: React, TypeScript, GraphQL",
     },
     {
-      company: "Clevertech",
-      link: "https://clevertech.biz",
-      badges: ["Remote"],
-      title: "Lead Android Developer → Full Stack Developer",
-      logo: ClevertechLogo,
-      start: "2015",
+      company: "Cipher Tech Solutions",
+      link: "https://ciphertechsolutions.com",
+      badges: ["Washington, D.C."],
+      title: "Forensic Software Developer",
+      logo: CipherTechLogo,
+      start: "2017",
       end: "2021",
       description:
         "Created Android mobile apps and led teams for companies like Vision Media, DKMS, or AAA. Built live streaming application for Evercast from scratch. Technologies: Android, Kotlin, React, TypeScript, GraphQL",
     },
-    {
-      company: "Jojo Mobile",
-      link: "https://bsgroup.eu/",
-      badges: [],
-      title: "Android Developer → Lead Android Developer",
-      logo: JojoMobileLogo,
-      start: "2012",
-      end: "2015",
-      description:
-        "Built an Android team, created Android apps for biggest Polish companies like LOT, Polskie Radio, Agora, PolskaPress",
-    },
-    {
-      company: "Nokia Siemens Networks",
-      link: "https://www.nokia.com",
-      badges: [],
-      title: "C/C++ Developer",
-      logo: NSNLogo,
-      start: "2010",
-      end: "2012",
-      description: "Creating and testing software for LTE base stations",
-    },
   ],
   skills: [
+    "Java",
+    "Python",
+    "Go",
     "JavaScript",
     "TypeScript",
-    "React/Next.js/Remix",
+    "React/Next.js",
     "Node.js",
-    "GraphQL",
-    "Relay",
-    "WebRTC",
+    "Postgres",
+    "SQL Server",
+    "Redis"
   ],
   projects: [
     {
-      title: "Consultly",
+      title: "WHOOP Coach",
       techStack: [
-        "Side Project",
-        "TypeScript",
-        "Next.js",
-        "Vite",
-        "GraphQL",
-        "WebRTC",
+        "Java", "LLMs", "Redis",
       ],
       description: "A platform to build and grow your online business",
-      logo: ConsultlyLogo,
+      logo: WHOOPCoachLogo,
       link: {
-        label: "consultly.com",
-        href: "https://consultly.com/",
+        label: "WHOOP Coach blog post",
+        href: "https://www.whoop.com/us/en/thelocker/introducing-whoop-coach-powered-by-openai/",
       },
     },
     {
-      title: "Monito",
-      techStack: ["Side Project", "TypeScript", "Next.js", "Browser Extension"],
+      title: "Mags Hot Sauce",
+      techStack: ["Side Project", "Shopify"],
       description:
-        "Browser extension that records everything happening in a web application",
-      logo: MonitoLogo,
+        "Craft Hot Sauce brand sold online and in retail in SF.",
+      logo: MagsLogo,
       link: {
-        label: "monito.dev",
-        href: "https://monito.dev/",
+        label: "mags.lol",
+        href: "https://mags.lol/",
       },
     },
     {
-      title: "Jarocki.me",
-      techStack: ["Side Project", "Next.js", "MDX"],
+      title: "Sous.AI",
+      techStack: [
+        "Side Project", "iOS", "LLMs", "Python"
+      ],
+      description: "A platform to talk through recipes via LLM",
+    },
+    {
+      title: "Murder Mystery Generator",
+      techStack: [
+        "Side Project", "LLMs", "Python"
+      ],
+      description: "Murder mystery game generator using LLMs",
+    },
+    {
+      title: "Drawtf",
+      techStack: ["Side Project", "React", "Python", "Deprecated"],
       description:
-        "My personal website and blog. Built with Next.js and Notion API",
-      logo: JarockiMeLogo,
+        "Online Telestrations",
       link: {
         label: "github.com",
         href: "https://jarocki.me/",
       },
     },
     {
-      title: "Minimal",
-      techStack: ["Side Project", "Next.js", "Puppeteer"],
+      title: "Maze or Bowie",
+      techStack: ["Side Project", "React"],
       description:
-        "Minimalist calendars, habit trackers and planners generator",
-      logo: Minimal,
+        "Guess that Phish song!",
       link: {
-        label: "useminimal.com",
-        href: "https://useminimal.com/",
+        label: "mazeorbowie.com",
+        href: "https://mazeorbowie.com/",
       },
     },
     {
-      title: "Barepapers",
-      techStack: ["Side Project", "Next.js", "Puppeteer"],
+      title: "Tribal Council",
+      techStack: ["Side Project", "React", "Python"],
       description:
-        "Generates beautiful wallpapers using random shapes and gradients",
-      logo: BarepapersLogo,
-      link: {
-        label: "barepapers.com",
-        href: "https://barepapers.com/",
-      },
+        "NFL Survivor Pool Pick Ranker",
     },
     {
-      title: "Year progress",
-      techStack: ["Side Project", "TypeScript", "Next.js"],
-      description: "Tracks current year progress and displays a countdown",
-      logo: YearProgressLogo,
-      link: {
-        label: "getyearprogress.com",
-        href: "https://getyearprogress.com/",
-      },
-    },
-    {
-      title: "Parabol",
-      techStack: [
-        "Full Stack Developer",
-        "TypeScript",
-        "React",
-        "Node.js",
-        "GraphQL",
-      ],
+      title: "Party GIF",
+      techStack: ["Side Project", "Python"],
       description:
-        "The Agile meeting co-pilot that delivers better meetings with less effort",
-      logo: ParabolLogo,
-      link: {
-        label: "github.com",
-        href: "https://parabol.co/",
-      },
-    },
-    {
-      title: "Evercast",
-      techStack: [
-        "Lead Frontend Developer",
-
-        "TypeScript",
-        "React",
-        "Node.js",
-        "GraphQL",
-      ],
-      description:
-        "Creative collaboration platform that combines video conferencing and HD media streaming",
-      logo: EvercastLogo,
-      link: {
-        label: "evercast.us",
-        href: "https://www.evercast.us/",
-      },
-    },
-    {
-      title: "Mobile Vikings",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application for leading virtual mobile operator in Poland",
-      logo: MobileVikingsLogo,
-      link: {
-        label: "mobilevikings.pl",
-        href: "https://mobilevikings.pl/",
-      },
-    },
-    {
-      title: "Howdy",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Howdy is a place for you to join communities you care about",
-      logo: Howdy,
-      link: {
-        label: "play.google.com",
-        href: "https://howdy.co/",
-      },
-    },
-    {
-      title: "Tastycloud",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application for managing and displaying restaurant menus in kiosk mode",
-      logo: TastyCloudLogo,
-      link: {
-        label: "tastycloud.fr",
-        href: "https://www.tastycloud.fr/",
-      },
-    },
-    {
-      title: "Ambit",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application that helps with sharing your contact details",
-      logo: AmbitLogo,
-    },
-    {
-      title: "Bim",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application that helps with booking a table in a restaurants",
-      logo: BimLogo,
-    },
-    {
-      title: "Canal Digital GO",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Video streaming mobile application for Canal Digital subscribers",
-      logo: CDGOLogo,
+        "Turn any image into a party gif",
     },
   ],
 } as const;
--- src/images/logos/index.ts
+++ src/images/logos/index.ts
@@ -1,37 +1,11 @@
-import ConsultlyLogo from "./consultly.svg";
-import AmbitLogo from "./ambit.png";
-import BarepapersLogo from "./barepapers.svg";
-import BimLogo from "./bim.png";
-import CDGOLogo from "./cdgo.png";
-import ClevertechLogo from "./clevertech.png";
-import EvercastLogo from "./evercast.svg";
-import Howdy from "./howdy.png";
-import JarockiMeLogo from "./jarocki.svg";
-import JojoMobileLogo from "./jojomobile.png";
-import MonitoLogo from "./monito.svg";
-import MobileVikingsLogo from "./mv.png";
-import NSNLogo from "./nsn.svg";
-import ParabolLogo from "./parabol.svg";
-import TastyCloudLogo from "./tastycloud.png";
-import YearProgressLogo from "./yearprogress.svg";
-import Minimal from "./minimal.svg";
+import CipherTechLogo from "./CipherTechLogo.png";
+import MagsLogo from "./MagsLogo.png";
+import WHOOPCoachLogo from "./WHOOPCoachLogo.png";
+import WHOOPLogo from "./WHOOPLogo.png";
 
 export {
-  ConsultlyLogo,
-  AmbitLogo,
-  BarepapersLogo,
-  BimLogo,
-  CDGOLogo,
-  ClevertechLogo,
-  EvercastLogo,
-  Howdy,
-  JarockiMeLogo,
-  JojoMobileLogo,
-  MonitoLogo,
-  MobileVikingsLogo,
-  NSNLogo,
-  ParabolLogo,
-  TastyCloudLogo,
-  YearProgressLogo,
-  Minimal,
+  CipherTechLogo,
+  MagsLogo,
+  WHOOPCoachLogo,
+  WHOOPLogo
 };
