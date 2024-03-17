d703d1645d4daae92290c1ba7ff3f5f29ab0e51f
Alec White <alexander.k.white@gmail.com>
2024-03-17 07:19:00 -0700

    fix export

--- src/images/logos/index.ts
+++ src/images/logos/index.ts
@@ -2,10 +2,14 @@ import CipherTechLogo from "./CipherTechLogo.jpg";
 import MagsLogo from "./MagsLogo.png";
 import WHOOPCoachLogo from "./WHOOPCoachLogo.png";
 import WHOOPLogo from "./WHOOPLogo.png";
+import PromptForgeLogo from "./PromptForgeLogo.png";
+import SloothsLogo from "./SloothsLogo.png";
 
 export {
   CipherTechLogo,
   MagsLogo,
   WHOOPCoachLogo,
-  WHOOPLogo
+  WHOOPLogo,
+  PromptForgeLogo,
+  SloothsLogo
 };
