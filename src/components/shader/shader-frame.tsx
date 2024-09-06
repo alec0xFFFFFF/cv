9191b120ec027955c7804ed631a3fb3a0f73f687
Alec White <alexander.k.white@gmail.com>
2024-09-06 16:54:24 -0700

    fix shader

--- src/components/shader/shader-frame.tsx
+++ src/components/shader/shader-frame.tsx
@@ -1,14 +1,16 @@
 'use client';
-
 import * as React from 'react';
-
 import { useCanvasReactor } from './hooks/useCanvasReactor';
 
-export const ShaderFrame = () => {
+interface CanvasProps extends React.CanvasHTMLAttributes<HTMLCanvasElement> {
+  ref?: React.RefCallback<HTMLCanvasElement>;
+}
+
+export const ShaderFrame: React.FC = () => {
   const { register } = useCanvasReactor();
 
   return (
-    <canvas className="mx-auto" {...register()}>
+    <canvas className="mx-auto" {...register as CanvasProps}>
       Your browser does not support the HTML5 canvas tag.
     </canvas>
   );
